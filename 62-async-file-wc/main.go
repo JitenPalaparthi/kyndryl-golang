ad25f79b726cada04bafc816d8d7f88190d381d8
Jiten <jpalaparthi@gmail.com>
2024-04-01 15:04:32 +0530

    add example

--- 62-async-file-wc/main.go
+++ 62-async-file-wc/main.go
@@ -3,47 +3,63 @@ package main
 import (
 	"fmt"
 	"os"
-	"runtime"
 	"strings"
 )
 
 func main() {
+	done := make(chan struct{})
 
-	f, err := os.OpenFile("data1.txt", os.O_RDONLY, os.ModePerm)
+	f, err := os.OpenFile("data.txt", os.O_RDONLY, os.ModePerm)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1) // non zero exit is a fatal. Error from unix systems perspective
 	}
 	defer f.Close()
 	wcMap := make(map[int]int)
-	buf := make([]byte, 1)
-	lineNum := 1
-	var line string
-	for {
-		n, err := f.Read(buf)
-		if string(buf) == "\n" {
-			wc := WordCount(line)
-			//fmt.Println(wc)
-			if wc != 0 {
-				wcMap[lineNum] = wc
+	go func() {
+		buf := make([]byte, 1)
+		lineNum := 1
+		var line string
+
+		for {
+			n, err := f.Read(buf)
+			if string(buf) == "\n" {
+				chWC := WordCount(line) // go routine internally
+				v := <-chWC
+				if v != 0 {
+					wcMap[lineNum] = v
+				}
+				//}
+				line = ""
+				lineNum++
+			} else {
+				line = line + string(buf)
 			}
-			line = ""
-			lineNum++
+			if err != nil || n == 0 {
+				chWC := WordCount(line) // go routine internally
+				v := <-chWC
+				if v != 0 {
+					wcMap[lineNum] = v
+				}
+				done <- struct{}{}
+			}
 		}
-		line = line + string(buf)
-
-		if err != nil || n == 0 {
-			break
-		}
-	}
-
+	}()
+	<-done // Future pattern
 	fmt.Println(wcMap)
 
-	runtime.Goexit()
+	//runtime.Goexit()
+	//time.Sleep(time.Second * 5)
 }
 
-func WordCount(line string) int {
-	fmt.Println(line)
-	strs := strings.Fields(line)
-	return len(strs)
+func WordCount(line string) chan int {
+	countCh := make(chan int)
+	go func() {
+		strs := strings.Fields(line)
+		countCh <- len(strs)
+		close(countCh)
+	}()
+	return countCh
 }
+
+// map[1:7 2:11 4:4 5:4 6:15 7:15 8:6]
--- tasks/11-task.md
+++ tasks/11-task.md
@@ -1,2 +1,13 @@
 - Take example 62
 - enable go rountines and channels as and where possible
+
+- as soon as the work is done.. send to done channel which is a empty struct{}
+
+- add number of chars in a line
+- add number of chars that starts with vovel
+- add number of chars that ends with vovel
+- add number of special chars in a line
+- add number of vovels in a line
+- add number of consolnets in a line
+- add number of non ascii chars in a line
+- the file should be taken from command line argument
